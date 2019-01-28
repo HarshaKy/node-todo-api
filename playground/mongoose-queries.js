af7fce3610c4678ce3091820cea2e0440031629c
HarshaKy <HarshaKy>
2019-01-28 15:27:42 +0530

    asdf

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,20 +1,42 @@
 var {mongoose} = require('./../server/db/mongoose');
 var {Todo} = require('./../server/models/todo');
+var {User} = require('./../server/models/user');
 
-var id = '5c4ec35e488cfc14aa028c15';
+var id = '5c4ec5b37a229a1cd0301a86';
 
-Todo.find({
-  _id: id
-}).then((todos) => {
-  console.log('Todos', todos);
-});
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   // console.log('1st');
+//   if (todos.length === 0) {
+//     return console.log('Id not found');
+//   }
+//
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   // console.log('2nd');
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo', todo);
+// });
 
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log('Todo', todo);
-});
+// Todo.findById(id).then((todo) => {
+//   // console.log('3rd');
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo', todo);
+// }).catch((e) => console.log('Invalid Id'));
 
-Todo.findById(id).then((todo) => {
-  console.log('Todo', todo);
-});
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('User not found');
+  }
+
+  console.log('User', user);
+}).catch((e) => console.log('Invalid id/ user not found'));

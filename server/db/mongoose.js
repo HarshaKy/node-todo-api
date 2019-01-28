8d1b7949420f1072c49db8e8e2817164fbebdc4f
HarshaKy <harshaky6573@gmail.com>
2019-01-28 23:44:18 +0530

    asdf

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://HarshaKy:fibonacci12358!@ds143744.mlab.com:43744/deployment'
 };
-mongoose.connect( (db.localhost || db.mlab), {useNewUrlParser: true});
+mongoose.connect((process.env.PORT ? db.mlab : db.localhost), {useNewUrlParser: true});
 
 module.exports = {mongoose};
 

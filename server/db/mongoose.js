065c1420f7849edbc468b8aa7cc17ed459c36c02
HarshaKy <harshaky6573@gmail.com>
2019-01-29 23:26:21 +0530

    asdf

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://HarshaKy:fibonacci12358!@ds143744.mlab.com:43744/deployment'
+  mlab: 'mongodb://<UserName>:<password>@ds143744.mlab.com:43744/deployment'
 };
 mongoose.connect((process.env.PORT ? db.mlab : db.localhost), {useNewUrlParser: true});
 
--- server/server.js
+++ server/server.js
@@ -98,7 +98,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
-    body.comple = null;
+    body.completedAt = null;
   }
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {

fce1a60a75da1fbf3a2d88e6ebffd3093cf9d682
HarshaKy <harshaky6573@gmail.com>
2019-02-06 21:04:10 +0530

    setting up stuff for testing hashing passwords

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,6 +1,25 @@
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+  _id: userOneId,
+  email: 'harsha@gmail.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'charlie@gmail.com',
+  password: 'userTwoPass'
+}]
 
 const todos = [{
   _id: new ObjectID(),
@@ -18,4 +37,13 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-module.exports = {todos, populateTodos};
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
